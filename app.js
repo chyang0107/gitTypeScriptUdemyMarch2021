8dad334abe3459e9f709ee6eec838f13080415b6
chyang0107 <chyang0107@gmail.com>
2021-03-12 09:51:21 -0600

    typescript march 12

--- app.js
+++ app.js
@@ -2,5 +2,12 @@
 // console.log('hello world');
 // let greeting: string = 'hello'
 // type ExternalUserProfile = [number, string, string];
-var externalUserProfile = [1234, 'Robert', 'Peterson'];
+// let externalUserProfile: ExternalUserProfile = [1234, 'Robert', 'Peterson']
 // Lesson 2 Control Flow
+var score = 80;
+if (score >= 60) {
+    console.log('Pass');
+}
+else {
+    console.log('Fail');
+}
--- app.ts
+++ app.ts
@@ -3,5 +3,13 @@
 // let greeting: string = 'hello'
 
 // type ExternalUserProfile = [number, string, string];
-let externalUserProfile: ExternalUserProfile = [1234, 'Robert', 'Peterson']
+// let externalUserProfile: ExternalUserProfile = [1234, 'Robert', 'Peterson']
 // Lesson 2 Control Flow
+
+let score = 80;
+if(score>=60){
+    console.log('Pass');
+}
+else{
+    console.log('Fail');
+}
