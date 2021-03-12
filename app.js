d04a0a5e591e9bafc3b23efeff250a93b95fbe87
chyang0107 <chyang0107@gmail.com>
2021-03-12 09:45:18 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log('hello world');
+var greeting = 'hello';
+var externalUserProfile = [1234, 'Robert', 'Peterson'];
---
+++ app.ts
@@ -0,0 +1,6 @@
+console.log('hello world');
+
+let greeting: string = 'hello'
+//greeting = undefined;
+type ExternalUserProfile = [number, string, string];
+let externalUserProfile: ExternalUserProfile = [1234, 'Robert', 'Peterson']
