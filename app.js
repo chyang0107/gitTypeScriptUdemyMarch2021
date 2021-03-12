9a3c205239763021ab7e06b0acd25db314ed6db2
chyang0107 <chyang0107@gmail.com>
2021-03-12 09:46:27 -0600

    lesson2

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 "use strict";
-console.log('hello world');
-var greeting = 'hello';
+// console.log('hello world');
+// let greeting: string = 'hello'
+// type ExternalUserProfile = [number, string, string];
 var externalUserProfile = [1234, 'Robert', 'Peterson'];
+// Lesson 2 Control Flow
--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
-console.log('hello world');
+// console.log('hello world');
 
-let greeting: string = 'hello'
-//greeting = undefined;
-type ExternalUserProfile = [number, string, string];
+// let greeting: string = 'hello'
+
+// type ExternalUserProfile = [number, string, string];
 let externalUserProfile: ExternalUserProfile = [1234, 'Robert', 'Peterson']
+// Lesson 2 Control Flow
